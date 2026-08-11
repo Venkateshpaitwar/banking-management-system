bb1fe5bcd4a92c44d30deab7b455e931b5965b0d
Venkateshpaitwar <163770071+Venkateshpaitwar@users.noreply.github.com>
2026-08-11 17:01:02 +0530

    feat: add bank customer management

--- src/main/java/com/practice/banking/Bank.java
+++ src/main/java/com/practice/banking/Bank.java
@@ -1,4 +1,26 @@
 package com.practice.banking;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Bank {
+
+    private List<Customer> customersList = new ArrayList<>();
+    public void addCustomer(Customer customer) {
+        customersList.add(customer);
+    }
+
+    public void removeCustomer(Customer customer) {
+        customersList.remove(customer);
+    }
+
+    public Customer findByCustomerId(int customerId) {
+        for (Customer customer : customersList) {
+            if (customer.getCustomerId() == customerId) {
+                return customer;
+            }
+        }
+
+        return null;
+    }
 }
--- src/main/java/com/practice/banking/Main.java
+++ src/main/java/com/practice/banking/Main.java
@@ -3,14 +3,25 @@ package com.practice.banking;
 public class Main {
 
     public static void main(String[] args) {
+
         Customer c1 = new Customer(1, "Venkatesh", "vp@gmail.com");
         Customer c2 = new Customer(2, "Srushti", "sa@gmail.com");
-        System.out.println(c1.getCustomerId());
-        System.out.println(c1.getName());
-        System.out.println(c1.getEmail());
 
-        System.out.println(c2.getCustomerId());
-        System.out.println(c2.getName());
-        System.out.println(c2.getEmail());
+        Bank bank = new Bank();
+
+        bank.addCustomer(c1);
+        bank.addCustomer(c2);
+
+        Customer foundCustomer = bank.findByCustomerId(1);
+
+        if (foundCustomer != null) {
+            System.out.println("Customer found:");
+            System.out.println("ID: " + foundCustomer.getCustomerId());
+            System.out.println("Name: " + foundCustomer.getName());
+            System.out.println("Email: " + foundCustomer.getEmail());
+        } else {
+            System.out.println("Customer not found.");
+        }
+
     }
 }
